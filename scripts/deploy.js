69f02aaab726836123cc6d5502761238a47d8aee
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-05-11 19:00:59 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy DummyEthToken contract
+  const DummyEthToken = await ethers.getContractFactory("DummyEthToken");
+  const dummyEthToken = await DummyEthToken.deploy();
+  await dummyEthToken.deployed();
+  console.log("DummyEthToken deployed to:", dummyEthToken.address);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // Deploy DummyPolyToken contract
+  const DummyPolyToken = await ethers.getContractFactory("DummyPolyToken");
+  const dummyPolyToken = await DummyPolyToken.deploy();
+  await dummyPolyToken.deployed();
+  console.log("DummyPolyToken deployed to:", dummyPolyToken.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Deploy EthereumPortal contract
+  const EthereumPortal = await ethers.getContractFactory("EthereumPortal");
+  const ethereumPortal = await EthereumPortal.deploy(dummyEthToken.address);
+  await ethereumPortal.deployed();
+  console.log("EthereumPortal deployed to:", ethereumPortal.address);
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // Deploy PolygonPortal contract
+  const PolygonPortal = await ethers.getContractFactory("PolygonPortal");
+  const polygonPortal = await PolygonPortal.deploy(dummyPolyToken.address);
+  await polygonPortal.deployed();
+  console.log("PolygonPortal deployed to:", polygonPortal.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
